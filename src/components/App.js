38a83c104e5999cd448157db9ff9ab0ede317def
Pravin Bansod <136956809+PravinBansod65@users.noreply.github.com>
2024-03-22 05:23:50 +0000

    sdf

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-let input = { temperature: 25, conditions: "Sunny" };
+  let input = { temperature: 25, conditions: "Sunny" };
 
   return (
     <div>
-        {/* Do not remove the main div */}
-    <p style={input.temperature > 20? {color:"red"} : {color:"blue"} }>Temperature: {input.temperature}</p>
-        
+      {/* Do not remove the main div */}
+      <p>Temperature: <span style={input.temperature > 20 ? { color: "red" } : { color: "blue" }}>{input.temperature}</span> </p>
+
+
     </div>
   )
 }
