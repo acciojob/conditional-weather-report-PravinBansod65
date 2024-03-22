44b601aa53d35d20e5e29ad5f3fa9aecd3293d5b
Pravin Bansod <136956809+PravinBansod65@users.noreply.github.com>
2024-03-22 05:17:15 +0000

    sd

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ let input = { temperature: 25, conditions: "Sunny" };
   return (
     <div>
         {/* Do not remove the main div */}
-    <p style={input.temperature > 20? {color:"red"} : {color:"blue"} }> Temperature: {input.temperature}</p>
+    <p style={input.temperature > 20? {color:"red"} : {color:"blue"} }>Temperature: {input.temperature}</p>
         
     </div>
   )
