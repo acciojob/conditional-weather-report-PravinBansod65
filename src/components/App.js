b84b61ea64420f031575f6b156d315d4487492b5
Pravin Bansod <136956809+PravinBansod65@users.noreply.github.com>
2024-03-22 05:08:36 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+let input = { temperature: 25, conditions: "Sunny" };
+
   return (
     <div>
         {/* Do not remove the main div */}
+    <h2  style={input.temperature > 20? {color:"red"} : {color:"blue"} }> temperature : { input.temperature} </h2>
+        
     </div>
   )
 }
